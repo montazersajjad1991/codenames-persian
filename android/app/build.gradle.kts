fc49b897c099e231d5daadbcf6db8e122a3bbe25
montazersajjad1991 <montazersajjad@gmail.com>
2026-09-13 13:28:26 +0330

    fix: finalize Kotlin 1.9.24 and clean workflow

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,17 +1,22 @@
 plugins {
     id("com.android.application")
+    id("org.jetbrains.kotlin.android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
     namespace = "com.example.codenames_persian"
-    compileSdk = 36
-    ndkVersion = "28.2.13676358"
+    compileSdk = 35
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlin {
+        jvmToolchain(17)
     }
 
     defaultConfig {
@@ -28,8 +33,6 @@ android {
         }
     }
 }
-
-
 
 flutter {
     source = "../.."
